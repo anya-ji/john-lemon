0e466d5e61f2b81823d87c687974a79054292a93
Anya Ji <aj592@cornell.edu>
2020-05-03 23:28:30 -0400

    finished tutorial + add diamonds

---
+++ 3D_Beginner/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public AudioSource caughtAudio;
+
+    bool m_IsPlayAtExit;
+    float m_Timer;
+    bool m_IsPlayerCaught;
+    bool m_HasAudioPlayed;
+
+    void OntriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayAtExit = true;
+        }
+    }
+
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_IsPlayAtExit)
+        {
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
+        }
+    }
+
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+    {
+        if(!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
---
+++ 3D_Beginner/Assets/Scripts/GetCrystal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetCrystal : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
